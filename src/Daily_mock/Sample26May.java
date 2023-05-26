20900cf0d7a79da9b3d4d35945ca620eecd79cf6
91738 <91738@DESKTOP-JE2TPP7>
2023-05-26 21:03:45 +0530

    [GAURI]: Sample26May code change

---
+++ src/Daily_mock/Sample26May.java
@@ -0,0 +1,13 @@
+package Daily_mock;
+
+public class Sample26May 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+		
+		System.out.println("Welcome to the world of selenium");
+		
+	}
+
+}
